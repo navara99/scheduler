2ca991623ec849fcf34b54ae83e75f01cb253ba5
Tharsikan <thar.navaratnam@gmail.com>
2021-12-07 13:53:28 -0500

    refactor event handler

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,16 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList({ days, day, setDay }) {
+export default function DayList({ days, value, onChange }) {
 
   const dayListItems = days.map((days) => {
     const { id, name, spots } = days;
-
-    return <DayListItem key={id} selected={name === day} setDay={setDay} spots={spots} name={name} />
+    return <DayListItem
+      key={id}
+      selected={name === value}
+      setDay={onChange}
+      spots={spots}
+      name={name} />
   })
 
 
