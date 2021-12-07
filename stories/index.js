13bef4f5c43507a01b62f83ab4184dd3f935e8f8
Tharsikan <thar.navaratnam@gmail.com>
2021-12-07 15:01:08 -0500

    add stories for Status component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 storiesOf("Button", module)
   .addParameters({
@@ -153,8 +154,9 @@ storiesOf("Appointment", module)
     interviewer={interviewer.name}
   />
   )
-  .add("Confirm", () => <Confirm 
-  message={"Delete the appointment?"} 
-  onConfirm={action("onConfirm")}
-  onCancel={action("onCancel")}
-   />)
+  .add("Confirm", () => <Confirm
+    message={"Delete the appointment?"}
+    onConfirm={action("onConfirm")}
+    onCancel={action("onCancel")}
+  />)
+  .add("Status", () => <Status message={"Deleting"} />)
