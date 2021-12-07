2b8f3cbed147155af47182a21ffd691d71e7ffb0
Tharsikan <thar.navaratnam@gmail.com>
2021-12-07 14:34:29 -0500

    add component for empty

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+export default function Empty({ onAdd }) {
+
+
+
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={onAdd}
+      />
+    </main>
+  )
+}
