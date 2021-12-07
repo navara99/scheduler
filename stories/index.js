802674c3c1db905b4d9c01f183791962a8a78b59
Tharsikan <thar.navaratnam@gmail.com>
2021-12-07 16:25:51 -0500

    add story for booked and empty appointment

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -165,3 +166,19 @@ storiesOf("Appointment", module)
   .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />)
   .add("Edit", () => <Form student="Thar" interviewer={3} interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
   .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")} />)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
