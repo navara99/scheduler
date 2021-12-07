ab5a28b6930a4af71916c32b877f0da43cb4f4f9
Tharsikan <thar.navaratnam@gmail.com>
2021-12-07 16:06:36 -0500

    add state to Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,15 +4,23 @@ import Button from "components/Button";
 
 
 export default function Form({ student, interviewer, interviewers, onSave, onCancel }) {
-  const [currentStudent, setCurrentStudent] = useState(student);
+  const [currentStudent, setCurrentStudent] = useState(student || "");
+  const [currentInterviewer, setCurrentInterviewer] = useState(interviewer || null);
 
+  const reset = () => {
+    setCurrentInterviewer("");
+    setCurrentStudent("");
+  };
 
-
+  const cancel = () => {
+    onCancel();
+    reset();
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(e) => e.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -23,14 +31,15 @@ export default function Form({ student, interviewer, interviewers, onSave, onCan
           />
         </form>
         <InterviewerList
-          value={interviewer}
+          value={currentInterviewer}
           interviewers={interviewers}
+          onChange={setCurrentInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={onCancel}>Cancel</Button>
-          <Button confirm onClick={onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => onSave(currentStudent, currentInterviewer)}>Save</Button>
         </section>
       </section>
     </main>
