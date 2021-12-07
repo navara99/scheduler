967bbbc6904e5622898d7451f44009c12b410a2a
Tharsikan <thar.navaratnam@gmail.com>
2021-12-07 12:10:12 -0500

    working InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,14 +1,24 @@
 import React from "react";
-import "components/InterviewerList";
+import "components/InterviewerList.scss";
+import InterviewListItem from "./InterviewListItem";
 
 export default function InterviewerList({ interviewers, setInterviewer, interviewer }) {
-
-
+  const interviewerItems = interviewers.map((interviewerItem) => {
+    const { id, name, avatar } = interviewerItem;
+    return <InterviewListItem
+      key={id}
+      id={id}
+      name={name}
+      avatar={avatar}
+      selected={id === interviewer}
+      setInterviewer={setInterviewer}
+    />
+  });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewerItems}</ul>
     </section>
   )
 }
