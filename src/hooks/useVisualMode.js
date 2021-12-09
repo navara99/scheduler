4f3e41ba8d9625eaf351afe71f63d605dd38aad2
Tharsikan <thar.navaratnam@gmail.com>
2021-12-09 18:04:29 -0500

    add transition with replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,18 +4,14 @@ const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (next) => {
-    setHistory((prev) => [...prev, next]);
+  const transition = (next, replace = false) => {
+    setHistory((prev) => replace ? [...prev.slice(0, prev.length - 1), next] : [...prev, next]);
     setMode(next);
   }
 
   const back = () => {
     if (history.length === 1) return
-    setHistory((prev) => {
-      const removedSelfInHistory = [...prev];
-      removedSelfInHistory.pop();
-      return removedSelfInHistory;
-    });
+    setHistory((prev) => prev.slice(0, prev.length - 1));
     const last = history[history.length - 2];
     setMode(last);
   }
