4110fd1d0f9fde24bad01485e8f5217e85c700ae
Tharsikan <thar.navaratnam@gmail.com>
2021-12-09 18:19:36 -0500

    conditionally render form with useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,21 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment({ time, interview, id }) {
-
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const { transition, back, mode } = useVisualMode(interview ? SHOW : EMPTY);
 
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} time={time} /> : <Empty />}
+      {mode === SHOW && < Show student={interview.student} interviewer={interview.interviewer.name} time={time} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
     </article>
   )
 
