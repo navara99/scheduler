6c609c10e524d53007b3b14b9795978c0c209dca
Tharsikan <thar.navaratnam@gmail.com>
2021-12-09 19:23:00 -0500

    pass interviewers for the day to form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -6,7 +6,7 @@ import Empty from "./Empty";
 import Form from "./Form";
 import useVisualMode from "hooks/useVisualMode";
 
-export default function Appointment({ time, interview, id }) {
+export default function Appointment({ time, interview, interviewers }) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
@@ -17,7 +17,7 @@ export default function Appointment({ time, interview, id }) {
       <Header time={time} />
       {mode === SHOW && < Show student={interview.student} interviewer={interview.interviewer.name} time={time} />}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
+      {mode === CREATE && <Form interviewers={interviewers} onCancel={() => back()} />}
     </article>
   )
 
