a743c50c8f19f79313b4580e9c5ac1b3668c0f1c
Tharsikan <thar.navaratnam@gmail.com>
2021-12-09 19:22:04 -0500

    pass interviewersForDay into Appointment components

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import "components/Application.scss";
 
 export default function Application(props) {
@@ -20,15 +20,22 @@ export default function Application(props) {
         axios.get(interviewersEndpoint)
       ]
     )
-    .then(([{ data: days }, { data: appointments }, { data: interviewers }]) => {
-      setState(({ days, appointments, interviewers }));
-    })
-    .catch((err) => console.error(err.message));
+      .then(([{ data: days }, { data: appointments }, { data: interviewers }]) => {
+        setState(({ days, appointments, interviewers }));
+      })
+      .catch((err) => console.error(err.message));
   }, []);
 
+  const interviewersForDay = getInterviewersForDay(state, day);
+  
   const appointmentList = getAppointmentsForDay(state, day).map((appointment) => {
     const transformedInterview = getInterview(state, appointment.interview);
-    return <Appointment key={appointment.id} time={appointment.time} interview={transformedInterview} />
+
+    return <Appointment
+      key={appointment.id}
+      time={appointment.time}
+      interviewers={interviewersForDay}
+      interview={transformedInterview} />
   }).concat(<Appointment key="last" time="5pm" />);
 
   return (
