50b9b670b03f4cbb7ce99a46a2b7b04734653422
Tharsikan <thar.navaratnam@gmail.com>
2021-12-13 14:13:21 -0500

    show loadinganimation on post request

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,21 +4,29 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment({ time, interview, interviewers, bookInterview, id }) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   const { transition, back, mode } = useVisualMode(interview ? SHOW : EMPTY);
 
-  const save = (name, interviewer) => {
+  const save = async (name, interviewer) => {
     const interview = {
       student: name,
       interviewer
     }
-    bookInterview(id, interview);
-    transition("SHOW");
+    transition("SAVING");
+    try {
+      await bookInterview(id, interview);
+      transition("SHOW");
+    } catch (e) {
+      console.log(e.message);
+    }
+
   };
 
   return (
@@ -27,6 +35,7 @@ export default function Appointment({ time, interview, interviewers, bookIntervi
       {mode === SHOW && < Show student={interview.student} interviewer={interview.interviewer.name} time={time} />}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && <Form onSave={save} interviewers={interviewers} onCancel={() => back()} />}
+      {mode === SAVING && <Status />}
     </article>
   )
 
