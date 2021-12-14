ec3976ca949f95058a1a922cd9cf5dddc979df2a
Tharsikan <thar.navaratnam@gmail.com>
2021-12-14 15:42:33 -0500

    setup test

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "../Appointment/index"
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+
+  
+
+
+})
---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Form from "components/Appointment/Form";
+
+afterEach(cleanup);
+
+describe("Form", () => {
+  const interviewers = [
+    {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    }
+  ];
+
+  it("renders without student name if not provided", () => {
+    const { getByPlaceholderText } = render(<Form />)
+    expect(getByPlaceholderText("Enter Student Name")).toHaveValue("");
+  });
+
+  it("renders with initial student name", () => {
+    const { getByTestId } = render(<Form />);
+    expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
+  });
+});
