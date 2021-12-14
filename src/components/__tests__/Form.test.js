005a1f95d42249f4a3314dd1ef3ee85baa9d3134
Tharsikan <thar.navaratnam@gmail.com>
2021-12-14 16:31:41 -0500

    add tests for happy path on form submit

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -51,4 +51,19 @@ describe("Form", () => {
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form {... { interviewers }} {...{ onSave }} />
+    );
+
+    const input = getByPlaceholderText("Enter Student Name");
+
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
+  });
+
 });
