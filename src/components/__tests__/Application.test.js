a9dfe54e95d0d4d0aa1cf5653819040471df72f4
Tharsikan <thar.navaratnam@gmail.com>
2021-12-14 20:03:59 -0500

    change query search to regex

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -35,23 +35,23 @@ describe("Application", () => {
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
     const { container } = render(<Application />);
 
-    await waitForElement(() => getByText(container, "Archie Cohen"));
+    await waitForElement(() => getByText(container, /Archie Cohen/i));
 
-    const appointment = getAllByTestId(container, "appointment")[0];
+    const appointment = getAllByTestId(container, /appointment/i)[0];
 
-    fireEvent.click(getByAltText(appointment, "Add"));
+    fireEvent.click(getByAltText(appointment, /Add/i));
 
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
 
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByAltText(appointment, /Sylvia Palmer/i));
 
-    fireEvent.click(getByText(appointment, "Save"));
+    fireEvent.click(getByText(appointment, /Save/i));
 
     expect(getByText(appointment, /saving/i)).toBeInTheDocument();
 
-    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    await waitForElement(() => getByText(appointment, /Lydia Miller-Jones/i));
 
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
