7d495bdc42eb13e3b69a8297de6d092134b89e09
Tharsikan <thar.navaratnam@gmail.com>
2021-12-15 16:04:19 -0500

    add test - should edit an existing interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,6 +1,6 @@
 describe("Appointments", () => {
 
-  beforeEach(()=> {
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
 
     cy.visit("/");
@@ -9,16 +9,22 @@ describe("Appointments", () => {
   })
 
   it("should book an interview", () => {
-
     cy.get("[alt=Add]").first().click();
-
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
-
     cy.contains("Save").click();
-
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.contains(".appointment__card--show", "Archie Cohen");
+    cy.get("[alt=Edit]").first().click({ force: true });
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
 });
